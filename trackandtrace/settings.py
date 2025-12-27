839deaf11a397518fd6dcda373a49d39912dd70b
Dheeraj <f1r1e1s1h1@gmail.com>
2025-12-27 02:06:07 -0800

    Switch to PostgreSQL(Supabase)

--- trackandtrace/settings.py
+++ trackandtrace/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'trackandtrace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
