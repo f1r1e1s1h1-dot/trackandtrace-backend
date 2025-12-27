eae3f3b7ce5b376a347da8e4637eb00c9d4873bc
f1r1e1s1h1-dot <f1r1e1s1h1@gmail.com>
2025-12-27 05:10:29 -0800

    Add health endpoint and fix urlpatterns

--- trackandtrace/urls.py
+++ trackandtrace/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+def health(request):
+    return JsonResponse({"ok": True})
+    
 urlpatterns = [
     path('', lambda request: JsonResponse({"status":"ok"})),
     path('admin/', admin.site.urls),
     path('api/', include('track_api.urls')),   # ← THIS LINE WAS MISSING
+    path("health/", health),
 ]
