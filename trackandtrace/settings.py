11ac7231502128508afb77a16c3b87b4d53fdead
f1r1e1s1h1-dot <f1r1e1s1h1@gmail.com>
2026-01-24 05:22:45 -0800

    Fix DATABASE_URL retrieval in settings.py

--- trackandtrace/settings.py
+++ trackandtrace/settings.py
@@ -80,7 +80,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
+        os.environ.get("DATABASE_URL",""),
         conn_max_age=600,
         ssl_require=True,
     )
